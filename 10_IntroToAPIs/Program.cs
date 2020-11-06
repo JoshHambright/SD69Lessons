5a818d6d7c579b3dd38bfc501a9f20eb68475d34
JoshHambright <josh.hambright@gmail.com>
2020-11-06 11:26:11 -0500

    more API stuff, posting to an API

--- 10_IntroToAPIs/Program.cs
+++ 10_IntroToAPIs/Program.cs
@@ -1,8 +1,11 @@
 ﻿using _10_IntroToAPIs.Models;
+using _10_IntroToAPIs.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _10_IntroToAPIs
@@ -11,6 +14,58 @@ namespace _10_IntroToAPIs
     {
         static void Main(string[] args)
         {
+            MySwapiService mySwapiService = new MySwapiService();
+            Console.WriteLine("Enter a name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter a gender");
+            string gender = Console.ReadLine();
+            Console.WriteLine("Enter a homeworld ID");
+            int homeworld = Convert.ToInt32(Console.ReadLine());
+
+            Character newCharacter = new Character()
+            {
+                Name = name,
+                Gender = gender,
+                Homeworld = homeworld
+            };
+            Console.Clear();
+            Console.WriteLine("Saving new character...");
+            int statusCode = (int) mySwapiService.PostCharacterAsync(newCharacter).Result;
+
+            //mySwapiService.PostCharacterAsync(newCharacter).Wait();
+
+            Console.Clear();
+            if(statusCode == 200)
+            {
+            Console.WriteLine("\n\nNew Character Created!");
+
+            }
+            else
+            {
+                Console.WriteLine("\n\nError saving character." + statusCode);
+            }
+
+            Console.ReadKey();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter Character ID:");
+                int id = Int32.Parse(Console.ReadLine());
+                if (id == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("\n\nLoading...");
+                CharacterWithHomeworld character = mySwapiService.GetCharacterWithHomeworldAsync(id).Result;
+                //Planet planet = mySwapiService.GetPlanetAsync(luke.Homeworld).Result;
+                Console.WriteLine("\n\n");
+                Console.WriteLine(character.Name);
+                Console.WriteLine("\n\n");
+                Console.WriteLine(character.Homeworld.Name);
+                Console.ReadKey();
+            }
             //SwapiService swapiService = new SwapiService();
             //Person luke = swapiService.GetPersonAsynch(1).Result;
 
@@ -21,69 +76,69 @@ namespace _10_IntroToAPIs
             //DadJoke joke = dadJokeService.GetDadJoke().Result;
             //Console.WriteLine(joke.Joke);
 
-            TacoService tacoService = new TacoService();
-            TacoRecipe taco = tacoService.GetTacoRecipeAsync().Result;
-            bool keeprunning = true;
-            while (keeprunning)
-            {
+            //            TacoService tacoService = new TacoService();
+            //            TacoRecipe taco = tacoService.GetTacoRecipeAsync().Result;
+            //            bool keeprunning = true;
+            //            while (keeprunning)
+            //            {
 
-                Console.Clear();
-                Console.WriteLine(@"
-████████  █████   ██████  ██████  ███████ 
-   ██    ██   ██ ██      ██    ██ ██      
-   ██    ███████ ██      ██    ██ ███████ 
-   ██    ██   ██ ██      ██    ██      ██ 
-   ██    ██   ██  ██████  ██████  ███████ 
-");
-                Console.WriteLine(taco.Seasoning.Name);
-                Console.WriteLine(taco.Base_Layer.Name);
-                Console.WriteLine(taco.Condiment.Name);
-                Console.WriteLine(taco.Mixin.Name);
-                Console.WriteLine(taco.Shell.Name);
-                Console.WriteLine();
-                Console.WriteLine("1>Show Seasoning");
-                Console.WriteLine("2>Show Base Layer");
-                Console.WriteLine("3>Show Condiment");
-                Console.WriteLine("4>Show Mixin");
-                Console.WriteLine("5>Show Shell");
-                Console.WriteLine("6>Exit");
-                string menu = Console.ReadLine();
-                switch (menu)
-                {
-                    case "1":
-                        Console.Clear();
-                        Console.WriteLine(taco.Seasoning.Recipe);
-                        Console.ReadKey();
-                        break;
-                    case "2":
-                        Console.Clear();
-                        Console.WriteLine(taco.Base_Layer.Recipe);
-                        Console.ReadKey();
-                        break;
-                    case "3":
-                        Console.Clear();
-                        Console.WriteLine(taco.Condiment.Recipe);
-                        Console.ReadKey();
-                        break;
-                    case "4":
-                        Console.Clear();
+            //                Console.Clear();
+            //                Console.WriteLine(@"
+            //████████  █████   ██████  ██████  ███████ 
+            //   ██    ██   ██ ██      ██    ██ ██      
+            //   ██    ███████ ██      ██    ██ ███████ 
+            //   ██    ██   ██ ██      ██    ██      ██ 
+            //   ██    ██   ██  ██████  ██████  ███████ 
+            //");
+            //                Console.WriteLine(taco.Seasoning.Name);
+            //                Console.WriteLine(taco.Base_Layer.Name);
+            //                Console.WriteLine(taco.Condiment.Name);
+            //                Console.WriteLine(taco.Mixin.Name);
+            //                Console.WriteLine(taco.Shell.Name);
+            //                Console.WriteLine();
+            //                Console.WriteLine("1>Show Seasoning");
+            //                Console.WriteLine("2>Show Base Layer");
+            //                Console.WriteLine("3>Show Condiment");
+            //                Console.WriteLine("4>Show Mixin");
+            //                Console.WriteLine("5>Show Shell");
+            //                Console.WriteLine("6>Exit");
+            //                string menu = Console.ReadLine();
+            //                switch (menu)
+            //                {
+            //                    case "1":
+            //                        Console.Clear();
+            //                        Console.WriteLine(taco.Seasoning.Recipe);
+            //                        Console.ReadKey();
+            //                        break;
+            //                    case "2":
+            //                        Console.Clear();
+            //                        Console.WriteLine(taco.Base_Layer.Recipe);
+            //                        Console.ReadKey();
+            //                        break;
+            //                    case "3":
+            //                        Console.Clear();
+            //                        Console.WriteLine(taco.Condiment.Recipe);
+            //                        Console.ReadKey();
+            //                        break;
+            //                    case "4":
+            //                        Console.Clear();
 
-                        Console.WriteLine(taco.Mixin.Recipe);
-                        Console.ReadKey();
-                        break;
-                    case "5":
-                        Console.Clear();
-                        Console.WriteLine(taco.Shell.Recipe);
-                        Console.ReadKey();
-                        break;
-                    
-                    case "6":
-                        keeprunning = false;
-                        break;
-                    default:
-                        break;
-                }
-            }
+            //                        Console.WriteLine(taco.Mixin.Recipe);
+            //                        Console.ReadKey();
+            //                        break;
+            //                    case "5":
+            //                        Console.Clear();
+            //                        Console.WriteLine(taco.Shell.Recipe);
+            //                        Console.ReadKey();
+            //                        break;
+
+            //                    case "6":
+            //                        keeprunning = false;
+            //                        break;
+            //                    default:
+            //                        break;
+            //                }
+            //            }
         }
     }
 }
---
+++ 10_IntroToAPIs/Services/MySwapiService.cs
@@ -0,0 +1,93 @@
+﻿using _10_IntroToAPIs.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_IntroToAPIs.Services
+{
+    public class MySwapiService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string baseUrl = "https://us-central1-lateral-incline-114906.cloudfunctions.net/swapi/";
+
+        public async Task<Character> GetCharacterAsynch(int id)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(baseUrl + "people/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                Character character = await response.Content.ReadAsAsync<Character>();
+                return character;
+            }
+            return null;
+        }
+
+        public async Task<Planet> GetPlanetAsync(int id)
+        {
+
+            HttpResponseMessage response = await _httpClient.GetAsync(baseUrl + "planets/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                Planet planet = await response.Content.ReadAsAsync<Planet>();
+                return planet;
+            }
+            return null;
+        }
+        public async Task<CharacterWithHomeworld> GetCharacterWithHomeworldAsync(int id)
+        {
+
+            HttpResponseMessage response = await _httpClient.GetAsync(baseUrl + "people/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                Character character = await response.Content.ReadAsAsync<Character>();
+                CharacterWithHomeworld characterWithHomeworld = new CharacterWithHomeworld()
+                {
+                    Name = character.Name,
+                    HomeworldId = character.Homeworld,
+                    Gender = character.Gender
+                };
+
+                HttpResponseMessage planetResponse = await _httpClient.GetAsync(baseUrl + "planets/" + character.Homeworld);
+                if (planetResponse.IsSuccessStatusCode)
+                {
+                    Planet homeworld = await planetResponse.Content.ReadAsAsync<Planet>();
+                    characterWithHomeworld.Homeworld = homeworld;
+                }
+                else
+                {
+                    characterWithHomeworld.Homeworld = null;
+                }
+
+                return characterWithHomeworld;
+            }
+            return null;
+        }
+
+        public async Task<HttpStatusCode> PostCharacterAsync(Character character)
+        {
+            //var json = JsonConvert.SerializeObject(character);
+            //var data = new StringContent(json, Encoding.UTF8, "application/json");
+            //HttpResponseMessage response = await _httpClient.PostAsync(baseUrl + "people/", data);
+
+            Dictionary<string, string> characterData = new Dictionary<string, string>
+            {
+                {"name", character.Name },
+                {"gender", character.Gender },
+                {"homeworld", character.Homeworld.ToString() }
+            };
+            HttpContent content = new FormUrlEncodedContent(characterData);
+            HttpResponseMessage response = await _httpClient.PostAsync(baseUrl + "people/", content);
+            return response.StatusCode;
+
+
+            //string result = response.Content.ReadAsStringAsync().Result;
+            //return result;
+        }
+    }
+
+}
